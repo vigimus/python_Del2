5b1c3e8f30ba6e0da7412735d1d605b578a6d5cb
vigimus <160164175+vigimus@users.noreply.github.com>
2024-11-20 21:03:40 +0100

    Add files via upload

---
+++ python_del2.py
@@ -0,0 +1,11 @@
+fuel = 5
+money = 8
+
+if fuel > 3:
+    print("It's ok")
+    print("You can drive downtown.")
+elif money > 10:
+    print("You should buy some gas.")
+else:
+    print("You better stay at home.")
+    print("What's next?")
